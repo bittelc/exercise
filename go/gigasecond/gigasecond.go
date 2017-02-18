b1c40df1b547eb719c62c8a227b1051eec76136b
Cole Bittel <cole.bittel@gmail.com>
2017-02-18 00:51:55 -0800

    Don't need struct for already imported package

--- go/gigasecond/gigasecond.go
+++ go/gigasecond/gigasecond.go
@@ -1,15 +1,15 @@
 // Package gigaseconds calculates the datetime after a given datetime in which 1e9 seconds have passed
 package gigasecond
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
 const testVersion = 4
 
-type time struct {
-	Time time
-}
-
 // API function.  It uses a type from the Go standard library.
 func AddGigasecond(time.Time) time.Time {
-
+	return time.Time
+	// wtf!
 }
