aa839074fa3739054214277c4e94ede0e185aadc
Cole Bittel <cole.bittel@gmail.com>
2017-02-18 00:41:14 -0800

    Try and scaffold out time struct

--- go/gigasecond/gigasecond.go
+++ go/gigasecond/gigasecond.go
@@ -1,10 +1,15 @@
-// Package clause.
+// Package gigaseconds calculates the datetime after a given datetime in which 1e9 seconds have passed
 package gigasecond
 
-// Constant declaration.
-const testVersion = ? // find the value in gigasecond_test.go
+import "time"
+
+const testVersion = 4
+
+type time struct {
+	Time time
+}
 
 // API function.  It uses a type from the Go standard library.
-func AddGigasecond(time.Time) time.Time
+func AddGigasecond(time.Time) time.Time {
 
-// Reviewers don't think much of stub comments.  Replace or remove.
+}
