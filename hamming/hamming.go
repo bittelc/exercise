e39ec2faea1b30f0b96718f0fe4a342184f00d26
Cole Bittel <cole.bittel@gmail.com>
2017-02-18 18:47:33 -0800

    Passing tests for hamming

--- hamming/hamming.go
+++ hamming/hamming.go
@@ -1,9 +1,23 @@
 //Packaging hamming calculates the hamming distance between two homologuos strings.
 package hamming
 
+import "fmt"
+
 const testVersion = 5
 
 // Distance takes two strings and compares their constituent characters.
 func Distance(a, b string) (int, error) {
 
+	if len(a) != len(b) {
+		return -1, fmt.Errorf("Unequal string lengths!")
+	}
+
+	mutations := 0
+	for i := 0; i < len(a); i++ {
+		if a[i] != b[i] {
+			mutations += 1
+		}
+	}
+
+	return mutations, nil
 }
