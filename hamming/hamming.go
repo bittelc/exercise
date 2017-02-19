c8b127a3ea606d3e5b106c44ee5bc6932d3fe290
Cole Bittel <cole.bittel@gmail.com>
2017-02-18 16:30:46 -0800

    documentation for hamming

--- hamming/hamming.go
+++ hamming/hamming.go
@@ -1,7 +1,9 @@
+//Packaging hamming calculates the hamming distance between two homologuos strings.
 package hamming
 
 const testVersion = 5
 
+// Distance takes two strings and compares their constituent characters.
 func Distance(a, b string) (int, error) {
-	// your code here!
+
 }
