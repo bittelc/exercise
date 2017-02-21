82bf9af946e5137e2eb66e90ef2f31ed7a57fffa
Cole Bittel <cole.bittel@gmail.com>
2017-02-21 06:59:30 -0800

    Now works with spaces

--- acronym/acronym.go
+++ acronym/acronym.go
@@ -4,12 +4,16 @@ package acronym
 
 import "strings"
 import "fmt"
+import "unicode"
 
 const testVersion = 2
 
 func Abbreviate(str string) string {
 	var acronym string
-	words := strings.Fields(str)
+	f := func(c rune) bool {
+		return !unicode.IsLetter(c)
+	}
+	words := strings.FieldsFunc(str, f)
 	for _, word := range words {
 		acronym += fmt.Sprintf("%c", word[0])
 	}
