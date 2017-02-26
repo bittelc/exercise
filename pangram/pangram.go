5cb16beaf820ca61567e854024a450e9b9687fe2
Cole Bittel <cole.bittel@gmail.com>
2017-02-26 14:02:42 -0800

    2 tests still failing

--- pangram/pangram.go
+++ pangram/pangram.go
@@ -1,26 +1,33 @@
 // Package pangram determines if a given string is a pangram or not
 package pangram
 
+import (
+	"fmt"
+	"regexp"
+	"strings"
+)
+
 const testVersion = 1
 
 // IsPangram takes a string and determines if it is a pangram, return true if so; false if not
-func IsPangram(string) bool {
+func IsPangram(str string) bool {
 	uniquity := 0
-	unique_letters := ""
+	var unique_letters string
+	is_ascii := regexp.MustCompile("[A-Z]")
 
-	/*
-		for each character in string {
-			if character is not in unique_letter {
-				unique_letter += character
-				uniquity++
-			}
+	for _, char := range strings.ToUpper(str) {
+		if !strings.Contains(unique_letters, fmt.Sprint(char)) &&
+			is_ascii.MatchString(fmt.Sprintf("%q", char)) {
+			unique_letters += fmt.Sprint(char)
+			uniquity++
 		}
+	}
 
-		if uniquity == 24 {
-			return true
-		}
-		return false
-	*/
+	if uniquity == 24 {
+		return true
+	}
+	fmt.Println(uniquity)
+	fmt.Printf("%q", unique_letters)
+	return false
 
-	return true
 }
