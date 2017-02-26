97f417a7942ad30edee5888b6b78a3ff09778478
Cole Bittel <cole.bittel@gmail.com>
2017-02-26 11:48:37 -0800

    Scaffold of idea for pangram

--- pangram/pangram.go
+++ pangram/pangram.go
@@ -1,5 +1,26 @@
+// Package pangram determines if a given string is a pangram or not
 package pangram
 
 const testVersion = 1
 
-func IsPangram(string) bool
+// IsPangram takes a string and determines if it is a pangram, return true if so; false if not
+func IsPangram(string) bool {
+	uniquity := 0
+	unique_letters := ""
+
+	/*
+		for each character in string {
+			if character is not in unique_letter {
+				unique_letter += character
+				uniquity++
+			}
+		}
+
+		if uniquity == 24 {
+			return true
+		}
+		return false
+	*/
+
+	return true
+}
