1cea40f2f46ef47768c896d53f75dc9296e59190
Cole Bittel <cole.bittel@gmail.com>
2017-02-28 20:07:03 -0800

    Basic switch pattern down, now to even out all regex for each case

--- bob/bob.go
+++ bob/bob.go
@@ -1,9 +1,39 @@
 // Package bob defines a lame teenager who responds like a teenager does
 package bob
 
+import "regexp"
+
 const testVersion = 2
 
 // Hey accepts a statement to Bob and responds with a teenager-response
-func Hey(str string) {
+func Hey(str string) string {
+
+	//isCaps := regexp.MustCompile("\\w*[A-Z]\\b")
+	//isCaps := regexp.MustCompile("\\b[A-Z, ,\\!]{1,}")
+	//isCaps := regexp.MustCompile("^[A-Z,0-9, ,\\!,\\?]*$")
+	isAllCaps := regexp.MustCompile("^[A-Z, ,\\!,\\?]*$")
+	isNumsAndCaps := regexp.MustCompile("(^[0-9,\\, ]{1,})+([A-Z,\\!]{1,}$)")
+	isSpCharsAndCaps := regexp.MustCompile("(^[ -`]*)+([A-Z]{1,})+([ -`]*$)")
+
+	isLatinSupplement := regexp.MustCompile("")
+
+	isQuestion := regexp.MustCompile("\\w*\\?")
+
+	switch {
+	case isAllCaps.MatchString(str):
+		return "Whoa, chill out!"
+	case isNumsAndCaps.MatchString(str):
+		return "Whoa, chill out!"
+	case isSpCharsAndCaps.MatchString(str):
+		return "Whoa, chill out!"
+
+	case isLatinSupplement.MatchString(str):
+		return "Whoa, chill out!"
+
+	case isQuestion.MatchString(str):
+		return "Sure."
+	default:
+		return "Whatever."
+	}
 
 }
