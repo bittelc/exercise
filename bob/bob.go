72e667f727ab7ffda412a33d1f9279adedd07e1a
Cole Bittel <cole.bittel@gmail.com>
2017-03-02 20:27:46 -0800

    All tests are now passing

--- bob/bob.go
+++ bob/bob.go
@@ -9,14 +9,17 @@ const testVersion = 2
 // Hey accepts a statement to Bob and responds with a teenager-response
 func Hey(str string) string {
 
-	isAllCaps := regexp.MustCompile("^[A-Z, ,\\!,\\?]*$")
+	isAllCaps := regexp.MustCompile("^[A-Z, ,\\!,\\?]{1,}$")
 	isNumsAndCaps := regexp.MustCompile("(^[0-9,\\, ]{1,})+([A-Z,\\!]{1,}$)")
 	isSpCharsAndCaps := regexp.MustCompile("(^[ -`]*)+([A-Z]{1,})+([ -`]*$)")
-	isLatinSupplement := regexp.MustCompile("^[A-Z,!,Ä,Ü]*$")
-	isQuestion := regexp.MustCompile("^.*\\?$")
-	isBlank := regexp.MustCompile("")
+	isLatinSupplement := regexp.MustCompile("^[A-Z,!,Ä,Ü]{1,}$")
+	isQuestion := regexp.MustCompile("^.*\\?{1,} *$")
+	isBlank := regexp.MustCompile("^[^A-z][ ,\\t,\\n,\\r,\\v,\u00a0,\u2002]*$")
 
 	switch {
+	case isBlank.MatchString(str),
+		len(str) == 0:
+		return "Fine. Be that way!"
 	case isAllCaps.MatchString(str):
 		return "Whoa, chill out!"
 	case isNumsAndCaps.MatchString(str):
@@ -27,8 +30,6 @@ func Hey(str string) string {
 		return "Whoa, chill out!"
 	case isQuestion.MatchString(str):
 		return "Sure."
-	case isBlank(str):
-		return "Fine. Be that way!"
 	default:
 		fmt.Println(len(str))
 		return "Whatever."
