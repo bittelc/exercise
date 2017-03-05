e40591381c6fa8622d7e2cafd905b2eae890a69a
Cole Bittel <cole.bittel@gmail.com>
2017-03-05 14:12:32 -0800

    Equilateral triangle tests passing

--- triangle/triangle.go
+++ triangle/triangle.go
@@ -21,8 +21,21 @@ var Sca = Kind{Sca: true}
 // KindFromSides accepts lenghts of sides of a triangle and classifies the triangle
 func KindFromSides(a, b, c float64) Kind {
 	triangle := NaT
+
 	lengths := []float64{a, b, c}
 	sort.Float64s(lengths)
+	short := lengths[0]
+	mid := lengths[1]
+	long := lengths[2]
+
+	if short == mid && mid == long {
+		triangle = Equ
+	}
+
 	fmt.Println(lengths)
+	fmt.Println(short)
+	fmt.Println(mid)
+	fmt.Println(long)
+
 	return triangle
 }
