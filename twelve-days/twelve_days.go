9cadbb65fc3a488bad3f35ba8e7f6eb5bc591291
Cole Bittel <cole.bittel@gmail.com>
2017-03-17 18:50:51 -0700

    Repeating a partridge but getting close

--- twelve-days/twelve_days.go
+++ twelve-days/twelve_days.go
@@ -1,17 +1,52 @@
 // Package twelve returns a single verse for any of the twelve days of Christmas
 package twelve
 
-import ()
+import (
+	"fmt"
+)
 
 const testVersion = 1
 
+type dailyGift struct {
+	day  string
+	gift string
+}
+
+var twelveGifts = []dailyGift{
+	{"first", "a Partridge in a Pear Tree."},
+	{"second", "two Turtle Doves"},
+	{"third", "three French Hens"},
+	{"fourth", "four Calling Birds"},
+	{"fifth", "five Gold Rings"},
+	{"sixth", "six Geese-a-Laying"},
+	{"seventh", "seven Swans-a-Swimming"},
+	{"eighth", "eight Maids-a-Milking"},
+	{"ninth", "nine Ladies Dancing"},
+	{"tenth", "ten Lords-a-Leaping"},
+	{"eleventh", "eleven Pipers Piping"},
+	{"twelfth", "twelve Drummers Drumming"},
+}
+
 // Verse returns a single verse corresponding to the i'th numbered verse
 func Verse(i int) string {
-	return "oh ya"
+	verse := verbiage(twelveGifts[i-1])
+
+	for j := i; j >= 1; j-- {
+		verse += twelveGifts[j-1].gift
+	}
+	if i != 1 {
+		verse += " and " + twelveGifts[0].gift
+	}
+
+	return verse
 }
 
 // Song returns the entire song, line seperating each verse
 func Song() string {
 	// Could potentially use a recurse loop here. Eg. string += Song(i+1)
 	return "string"
+}
+
+func verbiage(daysGift dailyGift) string {
+	return fmt.Sprintf("On the %s day of Christmas my true love gave to me, ", daysGift.day)
 }
