2a9b1d68fb5438452c6f8bdfa2cc60a61e1566f1
Cole Bittel <cole.bittel@gmail.com>
2017-03-18 10:39:10 -0700

    Touchup and document solution

--- accumulate/accumulate.go
+++ accumulate/accumulate.go
@@ -5,6 +5,7 @@ import ()
 
 const testVersion = 1
 
+// Accumulate applies a provided function (fn) to a slice of strings and returns the newly formed slice of strings
 func Accumulate(input []string, fn func(mod string) string) []string {
 	var applied []string
 	for _, str := range input {
