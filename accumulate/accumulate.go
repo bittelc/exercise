5f71de32ceb51ff758524d749d3e1dbf948795d4
Cole Bittel <cole.bittel@gmail.com>
2017-03-17 21:53:16 -0700

    Accumulate function accepting correct input parameters

---
+++ accumulate/accumulate.go
@@ -0,0 +1,9 @@
+// Package accumulate implements a given function provided to the package, against a series of arguments provided to the package
+package accumulate
+
+import ()
+
+const testVersion = 1
+
+func Accumulate(input []string, method func(mod string) string) {
+}
