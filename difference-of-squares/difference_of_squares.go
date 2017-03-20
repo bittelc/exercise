7cd229e8cffaf7e41166b367cd350fcdc2963d50
Cole Bittel <cole.bittel@c02pq1q8fvh8.attlocal.net>
2017-03-19 22:15:02 -0700

    WIP
    Basic structure of main package is definted; tests still not runnable though.

---
+++ difference-of-squares/difference_of_squares.go
@@ -0,0 +1,19 @@
+package diffsquares
+
+import (
+	math
+)
+
+const testVersion = 1
+
+func SumOfSquares(n int) int {
+
+}
+
+func SquareOfSums(n int) int {
+
+}
+
+func Difference(n int) int {
+
+}
