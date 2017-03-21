49ab013c9dd544841651ddbeb6918a0ff455236e
Cole Bittel <cole.bittel@gmail.com>
2017-03-20 21:25:59 -0700

    All tests passing

--- difference-of-squares/difference_of_squares.go
+++ difference-of-squares/difference_of_squares.go
@@ -1,19 +1,21 @@
 package diffsquares
 
 import (
-	math
+	"math"
 )
 
 const testVersion = 1
 
 func SumOfSquares(n int) int {
-
+	return n * (n + 1) * (2*n + 1) / 6
 }
 
 func SquareOfSums(n int) int {
-
+	base := (((n + 1) * n) / 2)
+	return base * base
 }
 
 func Difference(n int) int {
-
+	diff := float64(SquareOfSums(n) - SumOfSquares(n))
+	return int(math.Abs(diff))
 }
