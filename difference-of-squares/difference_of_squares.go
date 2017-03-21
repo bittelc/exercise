75de072be854b32ab9ba8a40a111d49e58d9840d
Cole Bittel <cole.bittel@gmail.com>
2017-03-20 21:30:13 -0700

    Cleanup gofmt and documentation

--- difference-of-squares/difference_of_squares.go
+++ difference-of-squares/difference_of_squares.go
@@ -1,3 +1,4 @@
+// Package diffsquares can calculate the sum of the square, the square of the sums, and the difference between the two
 package diffsquares
 
 import (
@@ -6,15 +7,18 @@ import (
 
 const testVersion = 1
 
+// SumOfSquares calculates Σ((n!)^2) for a provided n
 func SumOfSquares(n int) int {
 	return n * (n + 1) * (2*n + 1) / 6
 }
 
+// SquareOfSums calculates (Σ(n!))^2 for a provdied n
 func SquareOfSums(n int) int {
 	base := (((n + 1) * n) / 2)
 	return base * base
 }
 
+// Difference calculates the difference between the SumOfSquares and SquareOfSums for any n
 func Difference(n int) int {
 	diff := float64(SquareOfSums(n) - SumOfSquares(n))
 	return int(math.Abs(diff))
